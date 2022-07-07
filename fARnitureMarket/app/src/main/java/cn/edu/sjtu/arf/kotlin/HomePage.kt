5474e955a56f0d7583e63e25855a52d23c38cf83
Lonely52Hz <1440279963@qq.com>
2022-07-08 00:10:17 +0200

    Unsolved for getting UIDs

--- fARnitureMarket/app/src/main/java/cn/edu/sjtu/arf/kotlin/HomePage.kt
+++ fARnitureMarket/app/src/main/java/cn/edu/sjtu/arf/kotlin/HomePage.kt
@@ -5,10 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ListView
+import android.widget.Toast
+import androidx.databinding.ObservableArrayList
+import androidx.databinding.ObservableList
 import cn.edu.sjtu.arf.R
 import androidx.fragment.app.Fragment
 import cn.edu.sjtu.arf.kotlin.homepagehelper.HomeItemAdapter
+import cn.edu.sjtu.arf.kotlin.homepagehelper.HomeItemDisplayStore.getHomeItemDisplays
 import cn.edu.sjtu.arf.kotlin.homepagehelper.HomeItemDisplayStore.homeitemdisplays
+import cn.edu.sjtu.arf.kotlin.homepagehelper.HomeItemUIDStore.getHomeItemUIDs
+import cn.edu.sjtu.arf.kotlin.homepagehelper.HomeItemUIDStore.homeitemUIDs
 
 class HomePage : Fragment() {
     private lateinit var homeItemListView: ListView
@@ -16,18 +22,46 @@ class HomePage : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val layout: View = inflater.inflate(R.layout.page_home,container,false)
-        val context = context as NavigateActivity
-        homeItemAdapter = HomeItemAdapter(context, homeitemdisplays)
+
         homeItemListView = layout.findViewById(R.id.homeItemListView)
-        homeItemListView.setAdapter(homeItemAdapter)
 
         return layout
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //var idx = 0
 
+        val context = context as NavigateActivity
+        homeItemAdapter = HomeItemAdapter(context, homeitemdisplays)
 
+        homeItemListView.setAdapter(homeItemAdapter)
+        homeitemdisplays.addOnListChangedCallback(propertyObserver)
 
+        getHomeItemUIDs()
+
+        Toast.makeText(activity, homeitemUIDs.size.toString(), Toast.LENGTH_SHORT).show()
+    }
+
+    //override fun onActivityCreated(savedInstanceState: Bundle?) {
+    //    super.onActivityCreated(savedInstanceState)
+    //}
+
+    private val propertyObserver = object: ObservableList.OnListChangedCallback<ObservableArrayList<Int>>() {
+        override fun onChanged(sender: ObservableArrayList<Int>?) { }
+        override fun onItemRangeChanged(sender: ObservableArrayList<Int>?, positionStart: Int, itemCount: Int) { }
+        override fun onItemRangeInserted(
+            sender: ObservableArrayList<Int>?,
+            positionStart: Int,
+            itemCount: Int
+        ) {
+            println("onItemRangeInserted: $positionStart, $itemCount")
+            getActivity()?.runOnUiThread {
+                homeItemAdapter.notifyDataSetChanged()
+            }
+        }
+        override fun onItemRangeMoved(sender: ObservableArrayList<Int>?, fromPosition: Int, toPosition: Int,
+                                      itemCount: Int) { }
+        override fun onItemRangeRemoved(sender: ObservableArrayList<Int>?, positionStart: Int, itemCount: Int) { }
     }
 }
--- fARnitureMarket/app/src/main/java/cn/edu/sjtu/arf/kotlin/homepagehelper/HomeItemUIDStore.kt
+++ fARnitureMarket/app/src/main/java/cn/edu/sjtu/arf/kotlin/homepagehelper/HomeItemUIDStore.kt
@@ -2,12 +2,17 @@ package cn.edu.sjtu.arf.kotlin.homepagehelper
 
 import android.content.Context
 import android.util.Log
+import android.widget.Toast
 import com.android.volley.RequestQueue
-import com.android.volley.toolbox.JsonObjectRequest
+import okhttp3.*
+import okhttp3.MultipartBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import com.google.ar.core.dependencies.i
 import org.json.JSONArray
 import org.json.JSONException
 import org.json.JSONObject
+import java.io.IOException
 import kotlin.reflect.full.declaredMemberProperties
 
 object HomeItemUIDStore {
@@ -15,25 +20,36 @@ object HomeItemUIDStore {
     val homeitemUIDs: List<HomeItemUID> = _homeitemUIDs
     private val nFields = HomeItemUID::class.declaredMemberProperties.size
 
-    private lateinit var queue: RequestQueue
-    private const val serverUrl = "https://101.132.97.115/" //IP address needs to be changed
+    private const val serverUrl = "https://101.132.97.115/"
 
-    fun getHomeItemUIDs(context: Context, completion: () -> Unit) {
-        val getRequest = JsonObjectRequest(serverUrl+"fetch_home_products/",
-            { response ->
-                _homeitemUIDs.clear()
-                val fullidx = response.keys()
-                while (fullidx.hasNext()) {
-                    val idx = fullidx.next().toString()
-                    if (response.getString(idx).isNotEmpty()) {
-                        _homeitemUIDs.add(
-                            HomeItemUID(UID = response.getString(idx)))
-                    } else {
-                        Log.e("getHomeItemUIDs", "Received empty UID")
+    private val client = OkHttpClient()
+
+    fun getHomeItemUIDs() {
+        val request = Request.Builder().url(serverUrl+"fetch_home_products/").build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("getHomeItemUIDs", "Failed GET request")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                //Log.e("getHomeItemUIDs", "GET response")
+                if (response.isSuccessful) {
+                    val homeItemUIDReceived = try { JSONObject(response.body?.string() ?: "")} catch (e: JSONException) {JSONObject()}
+                    val fullKey = homeItemUIDReceived.keys()
+                    while (fullKey.hasNext()) {
+                        Log.e("getHomeItemUIDs", "Received UID")
+                        val idx = fullKey.next().toString()
+                        if (homeItemUIDReceived.getString(idx).isNotEmpty()) {
+                            _homeitemUIDs.add(
+                                HomeItemUID(UID = homeItemUIDReceived.getString(idx)))
+                            Log.e("getHomeItemUIDs", "Received UID")
+                        } else {
+                            Log.e("getHomeItemUIDs", "Received empty UID")
+                        }
                     }
                 }
-                completion()
-            }, { completion() }
-        )
+            }
+        })
     }
 }
